ac6ed2a8154945c596a12c0f8313a605c328841f
fjjca <807789468@qq.com>
2026-04-13 11:26:44 +0800

    学习并添加流式输出

--- SpringAI/SpringAIStart/src/main/java/org/example/springaistart/controller/ChatController.java
+++ SpringAI/SpringAIStart/src/main/java/org/example/springaistart/controller/ChatController.java
@@ -1,12 +1,19 @@
 package org.example.springaistart.controller;
 
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.deepseek.DeepSeekChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Function;
 
 @RestController
 @RequestMapping("/ai")
@@ -16,13 +23,53 @@ public class ChatController {
     private DeepSeekChatModel chatModel;
 
     @GetMapping("/generate")
-    public  String generate(@RequestParam(value = "message", defaultValue = "给我讲个笑话") String message){
+    public  String generate(@RequestParam(value = "message", defaultValue = "你是谁（简单回答）") String message){
 
         System.out.println("message = "+message);
 
+        //一次性问答
         String resp = chatModel.call(message);
 
         System.out.println("resp = "+resp);
+        return resp;
+    }
+    @GetMapping("/generateStream0")
+    public  Flux<ChatResponse> generateStream0(@RequestParam(value = "message", defaultValue = "你是谁（简单回答）") String message){
+
+        System.out.println("message = "+message);
+
+        //流式输出JSON格式
+        Prompt prompt = new Prompt(new UserMessage(message));
+        Flux<ChatResponse> stream = chatModel.stream(prompt);
+
+        System.out.println("返回内容 = "+stream);
+        return stream;
+    }
+    @GetMapping("/generateStream")
+    public  Flux<String> generateStream(
+            @RequestParam(value = "message", defaultValue = "你是谁（简单回答）") String message,
+            HttpServletResponse response
+    ){
+
+        response.setCharacterEncoding("UTF-8");
+        System.out.println("message = "+message);
+
+        //流式返回内容
+        Prompt prompt = new Prompt(new UserMessage(message));
+        Flux<ChatResponse> stream = chatModel.stream(prompt);
+
+        Flux<String> resp = stream.map(
+                chatResponse -> chatResponse.getResult().getOutput().getText()
+        ).doOnNext(text -> System.out.print(" " + text));
+
+//        Flux<String> resp=stream.map(new Function<ChatResponse, String>() {
+//            @Override
+//            public String apply(ChatResponse chatResponse) {
+//                return chatResponse.getResult().getOutput().getText();
+//            }
+//        });
+
+        System.out.println("返回内容 = "+resp);
         return resp;
     }
 }
